197c07ff30ad3c760e47cefbcaeef32ecb30c3c0
AntonellaRamos <a20211437@pucp.edu.pe>
2026-07-08 21:12:35 -0500

    Crea script para preparación de datos de la ENAHO 2025

---
+++ 02_scripts/02_preparar_datos.R
@@ -0,0 +1,11 @@
+# =========================================================
+# Proyecto: ENAHO 2025 - Procesamiento de datos sobre hogares
+# beneficiarios de programas de asistencia alimentaria en Perú
+# Script: 02_preparar_datos.R
+# Propósito: Preparar la base de datos para el análisis mediante
+# la selección y renombrado de variables, la inspección inicial
+# y el diagnóstico de valores perdidos.
+# Fuente: INEI - ENAHO 2025
+# Autor: Antonella Ramos
+# Fecha: 08/07/2026
+# =========================================================
