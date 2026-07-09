2bce4ae81ab107f6137c35b712ce2e5ccb8a5e74
AntonellaRamos <a20211437@pucp.edu.pe>
2026-07-09 00:27:54 -0500

    Crea script para el tratamiento de valores perdidos en variables de programas y FIES de la ENAHO 2025

---
+++ 02_scripts/03_tratamiento_nas.R
@@ -0,0 +1,10 @@
+# =========================================================
+# Proyecto: ENAHO 2025 - Procesamiento de datos sobre hogares
+# beneficiarios de programas de asistencia alimentaria en Perú
+# Script: 03_tratamiento_nas.R
+# Propósito: Diagnóstico por tipo de ausencia y tratamiento
+# de valores perdidos en variables de programas y FIES
+# Fuente: INEI - ENAHO 2025
+# Autor: Antonella Ramos
+# Fecha: 09/07/2026
+# =========================================================
