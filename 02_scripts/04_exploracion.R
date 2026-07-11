a02f910f183dcae0682dd645cdbf4152f73a0abe
AntonellaRamos <a20211437@pucp.edu.pe>
2026-07-10 23:36:06 -0500

    Crea script para explorar los datos de la ENAHO 2025

---
+++ 02_scripts/04_exploracion.R
@@ -0,0 +1,10 @@
+# =========================================================
+# Proyecto: ENAHO 2025 - Procesamiento de datos sobre hogares
+# beneficiarios de programas de asistencia alimentaria en Perú
+# Script: 04_exploracion.R
+# Propósito: Conversión de tipos, estadísticas descriptivas,
+# visualizaciones exploratorias y exploración bivariada
+# Fuente: INEI - ENAHO 2025
+# Autor: Antonella Ramos
+# Fecha: 10/07/2026
+# =========================================================
