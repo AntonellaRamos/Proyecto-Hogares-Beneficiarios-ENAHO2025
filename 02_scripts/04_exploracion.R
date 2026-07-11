56825294f53b8df0d0e9d6b13f22e3546ef58e27
AntonellaRamos <a20211437@pucp.edu.pe>
2026-07-10 23:39:53 -0500

    Inicializa la gestión de librerías para la exploración de datos

--- 02_scripts/04_exploracion.R
+++ 02_scripts/04_exploracion.R
@@ -8,3 +8,8 @@
 # Autor: Antonella Ramos
 # Fecha: 10/07/2026
 # =========================================================
+
+# 1. Cargar librerías ----
+library(tidyverse)
+library(arrow)
+library(gt)
