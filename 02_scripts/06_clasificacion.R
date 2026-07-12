3c8abad1a0c5cdc958225af003cdfe41a8d3cb6b
AntonellaRamos <a20211437@pucp.edu.pe>
2026-07-12 03:35:55 -0500

    Crea script para la recodificación de variables, construcción de beneficiario, índice FIES y tipologías de la ENAHO 2025

---
+++ 02_scripts/06_clasificacion.R
@@ -0,0 +1,10 @@
+# =========================================================
+# Proyecto: ENAHO 2025 - Procesamiento de datos sobre hogares
+# beneficiarios de programas de asistencia alimentaria en Perú
+# Script: 05_clasificacion.R
+# Propósito: Recodificación de variables, construcción de
+# variable de beneficiario, índice FIES y tipologías
+# Fuente: INEI - ENAHO 2025
+# Autor: Antonella Ramos
+# Fecha: 12/07/2026
+# =========================================================
