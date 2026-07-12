49edf883668b2b5cdc07f11ca16f3182ff6a20f2
AntonellaRamos <a20211437@pucp.edu.pe>
2026-07-12 05:29:26 -0500

    Crea script para la documentación de metadatos y generación del codebook final de la ENAHO 2025

---
+++ 02_scripts/08_documentar.R
@@ -0,0 +1,9 @@
+# ==============================================================================
+# Proyecto: ENAHO 2025 - Procesamiento de datos sobre hogares
+# beneficiarios de programas de asistencia alimentaria en Perú
+# Script: 08_documentar.R
+# Autor: Antonella Ramos
+# Fecha: 12/07/2026
+# Objetivo: Añadir metadatos a la base analítica y generar el codebook final.
+# ==============================================================================
+
