73f7b34435d284b4ce10f30389121b192fd68e2c
AntonellaRamos <a20211437@pucp.edu.pe>
2026-07-12 05:27:00 -0500

    Actualiza script de creación de carpetas y enlace GitHub para clasificar archivos por etapa de análisis dentro de la carpeta 03_outputs/

--- 00_creacion_carpetas_enlace_github.R
+++ 00_creacion_carpetas_enlace_github.R
@@ -6,6 +6,7 @@
 # y enlazar con GitHub
 # Autor: Antonella Ramos
 # Fecha: 08/07/2026
+# Ultima modificación: 12/07/2026
 # =========================================================
 
 # 1. Crear carpetas ----
@@ -14,7 +15,31 @@ dir.create("01_datos/originales")
 dir.create("01_datos/procesados")
 dir.create("02_scripts")
 dir.create("03_outputs")
+dir.create("03_outputs/acondicionar")
+dir.create("03_outputs/explorar")
+dir.create("03_outputs/clasificar")
+dir.create("03_outputs/documentar")
 
 # 2. Enlace con GitHub ----
 usethis::use_git()
 usethis::use_github()
+
+# 3. Mover archivos según prefijo ---
+mover_outputs <- function(prefijo, carpeta) {
+  
+  archivos <- list.files(
+    "03_outputs",
+    pattern = paste0("^", prefijo),
+    full.names = TRUE
+  )
+  
+  file.rename(
+    archivos,
+    file.path("03_outputs", carpeta, basename(archivos))
+  )
+}
+
+# Mover outputs
+mover_outputs("acondicionar", "acondicionar")
+mover_outputs("explorar", "explorar")
+mover_outputs("clasificar", "clasificar")
